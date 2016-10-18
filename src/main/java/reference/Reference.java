289c298fffbb798756b47e1ca6d589465a0dde3a
xelatercero <braga.alex57@gmail.com>
2016-10-18 09:21:14 +0200

    Add files via upload

---
+++ src/main/java/reference/Reference.java
@@ -0,0 +1,12 @@
+package reference;
+
+public class Reference {
+	
+	public static final String MOD_ID = "xelamod";
+	public static final String MOD_NAME = "xelamod";
+	public static final String VERSION = "1.10.2-1.0";
+	public static final String CLIENT_SIDE = "com.xelatercero.xelamod.ClientProxy";
+	public static final String SERVER_SIDE = "com.xelatercero.xelamod.ServerProxy";
+	
+	
+}
